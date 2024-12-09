74e02910637c6d6fabfa1c86330c13e08067f97b
Nahid <nahidthedeveloper@gmail.com>
2024-12-09 17:17:49 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const timeline_one = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#main",
+        // markers:true,
+        start: "38% 50%",
+        end: "100% 50%",
+        scrub: 2,
+        pin: true
+    }
+});
+timeline_one
+    .to(".text", {
+        top: "0",
+    }, 'a')
+    .to("#card-one", {
+        top: "35%",
+    }, 'a')
+    .to("#card-two", {
+        top: "130%"
+    }, 'a')
+    .to("#card-two", {
+        top: "42%"
+    }, 'b')
+    .to("#card-one", {
+        width: "65%",
+        height: "65vh"
+    }, 'b')
+    .to("#card-three", {
+        top: "130%"
+    }, 'b')
+    .to("#card-three", {
+        top: "50%"
+    }, 'c')
+    .to("#card-two", {
+        width: "70%",
+        height: "70vh"
+    }, 'c')
+
+
+window.addEventListener('mousemove', function (event) {
+    gsap.to('#cursor', {
+        x: event.x,
+        y: event.y,
+        ease: 'back.out',
+        autoAlpha: 1,
+        duration: 0.2
+    });
+});
